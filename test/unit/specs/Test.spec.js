4b83f04a54b4e90df6609bce06762cf565fae7d9
Nesh <albertovaccari108@gmail.com>
2017-05-10 00:22:43 +0900

    Added avoriaz library to ease testing button clicks

--- test/unit/specs/Test.spec.js
+++ test/unit/specs/Test.spec.js
@@ -1,11 +1,25 @@
-import Vue from 'vue'
+import { mount } from 'avoriaz';
 import Test from '@/components/Test'
 
 describe('Hello.vue', () => {
   it('renders data from the list', () => {
-    const Constructor = Vue.extend(Test)
-    const vm = new Constructor().$mount()
+    const test = mount(Test);
+    expect(test.text()).to.contain('welcome')
 
-    expect(vm.$el.textContent).to.contain('welcome')
+  })
+
+  it('adds entry to list on click', () => {
+
+    const test = mount(Test);
+    test.setData({
+    	list: ['my', 'list'],
+    	entry: 'myEntry'
+    });
+
+    const button = test.find('button')[0];
+    button.simulate('click');
+
+    expect(test.data().list).to.contain('myEntry');
+    expect(test.data().entry).to.equal('');
   })
 })
