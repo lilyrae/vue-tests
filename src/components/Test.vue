eb900cc483ce6a18128b585c9b073bad0ae9d01e
lilyrae <lily.bickerstaffe@live.co.uk>
2017-05-15 12:55:45 +0900

    Added validation to adding shopping list items

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -4,6 +4,7 @@
     </br>
     <input v-model="entry" >
     <button @click="addEntryToList">Click Me!</button>
+    <div v-if="error">{{error}}</div>
     <ul>
       <li v-for="item in list">{{ item }}</li>
     </ul>
@@ -16,13 +17,19 @@ export default {
   data () {
     return {
       list: ['hello', 'and', 'welcome'],
-      entry: ''
+      entry: '',
+      error: ''
     }
   },
   methods: {
       addEntryToList() {
-        this.list.push(this.entry);
-        this.entry = '';
+        if(this.entry != '') {
+          this.list.push(this.entry);
+          this.entry = '';
+        } else {
+          this.error = 'Please input your list item.'
+        }
+        
       }
   }
 }
