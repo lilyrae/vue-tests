5c123a6b189d67f76c3e87623d36db4aa2f63243
Nesh <albertovaccari108@gmail.com>
2017-05-19 13:07:06 +0900

    Renamed list component and deleted error message validation

---
+++ test/unit/specs/List.spec.js
@@ -0,0 +1,27 @@
+import { mount } from 'avoriaz';
+import List from '@/components/List'
+import Vue from 'vue'
+
+describe('List.vue', () => {
+
+  it('renders data from the list', () => {
+    const list = mount(List);
+    expect(test.text()).to.contain('play games')
+
+  })
+
+  it('adds new item to list on click', () => {
+
+    const list = mount(List);
+    list.setData({
+    	listItems: ['my', 'listItems'],
+    	newItem: 'mynewItem',
+    });
+
+    const button = list.find('button')[0];
+    button.simulate('click');
+
+    expect(list.data().listItems).to.contain('mynewItem');
+    expect(list.data().newItem).to.equal('');
+  })
+})
