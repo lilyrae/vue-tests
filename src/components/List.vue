e6ddc06fe7bd359560fc27f8fef059d50f2ae47a
lilyrae <lily.bickerstaffe@live.co.uk>
2017-05-19 13:07:06 +0900

    Renamed list component and deleted error message validation

---
+++ src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>My To Do List</h1>
+    </br>
+    <!-- user can add new item to the list --> 
+    <input v-model="newItem" >
+    <button @click="addItemToList">Add</button>
+    <ul>
+      <li v-for="item in listItems">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      listItems: ['buy food', 'play games', 'sleep'],
+      newItem: ''
+    }
+  },
+  methods: {
+      addItemToList() {
+        this.listItems.push(this.newItem);
+        this.newItem = '';
+      }
+  }
+}
+</script>
