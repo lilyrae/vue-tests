97fcea0f7a0594dd28ae96b292adfd951e75c8c1
lilyrae <lily.bickerstaffe@live.co.uk>
2017-05-19 13:10:20 +0900

    Updated router for new list element

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import Test from '@/components/Test'
+import List from '@/components/List'
 
 Vue.use(Router)
 
@@ -13,9 +13,9 @@ export default new Router({
       component: Hello
     },
     {
-      path: '/test',
-      name: 'test',
-      component: Test
+      path: '/to-do',
+      name: 'ToDoList',
+      component: List
     }
   ]
 })
