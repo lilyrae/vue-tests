8e48d3f0e34d8dc0b48606234770b96464483228
Lily Rae <lily.bickerstaffe@live.co.uk>
2017-06-08 00:39:11 +0800

    My first commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import List from '@/components/List'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/to-do',
+      name: 'ToDoList',
+      component: List
+    }
+  ]
+})
